b9d74063ec2158ed1d9226abd049b8c72c2a7f0f
Akop Karapetyan <dev@akop.org>
2007-07-27 01:52:12 +0000

    Atari800 runs! Check palette - colors seem off

--- config.h
+++ config.h
@@ -416,7 +416,7 @@
 /* #undef WORDS_BIGENDIAN */
 
 /* Define if unaligned word access is ok. */
-#define WORDS_UNALIGNED_OK 1
+/* #define WORDS_UNALIGNED_OK 1 */
 
 /* Target: Standard X11. */
 /* #undef X11 */
